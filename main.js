bdc26c2404e904dcaddd23ecd6ad033725284a2b
sadlikeyourdad <57995714+sadlikeyourdad@users.noreply.github.com>
2023-08-25 00:47:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Initialising AOS library
-    AOS.init();
-
-    const navbar = document.getElementById('navbar');
-    let scrolled = false;
-
-    window.onscroll = function() {
-        if (window.pageYOffset > 100) {
-            navbar.classList.add('scrolled');
-            if (!scrolled) {
-                navbar.style.transform = 'translateY(-70px)';
-            }
-            setTimeout(function() {
-                navbar.style.transform = 'translateY(0)';
-                scrolled = true;
-            }, 200);
-        } else {
-            navbar.classList.remove('scrolled');
-            scrolled = false;
-        }
-    }
+    AOS.init({
+        offset: 200, 
+        duration: 600, 
+        easing: 'ease-in-sine',
+        delay: 100
+    });
 });
-
-// If you have any other JS functions or interactions you'd like to add, you can include them below
-
-
-
