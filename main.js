8ff07b5c17dfe95278d54cb63bd3c390fa579f50
sadlikeyourdad <57995714+sadlikeyourdad@users.noreply.github.com>
2023-08-25 01:38:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ AOS.init({
 
 // Initialize the container with a fade-in effect
 document.querySelector('.container').setAttribute('data-aos', 'fade-up');
+// ... existing AOS and other script ...
+
+// Dark mode toggle
+const darkModeToggle = document.getElementById('darkModeToggle');
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('dark-mode', isDarkMode);
+});
+
+// Check local storage for dark mode setting
+window.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('dark-mode') === 'true') {
+        document.body.classList.add('dark-mode');
+    }
+});
