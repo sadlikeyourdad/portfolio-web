164f958f9c6d33cfe5a431482ac766984c2e00fe
sadlikeyourdad <57995714+sadlikeyourdad@users.noreply.github.com>
2023-08-24 22:54:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     AOS.init();
+    
+    const nav = document.querySelector("nav");
+    let scrolled = false;
+
+    window.onscroll = function() {
+        if (window.pageYOffset > 100) {
+            nav.classList.add('scrolled');
+            scrolled = true;
+        } else {
+            nav.classList.remove('scrolled');
+            scrolled = false;
+        }
+    };
 });
 
