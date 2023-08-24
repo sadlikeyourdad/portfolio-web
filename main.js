1df7e25ec3477a18331077f3664448dbf6e31b7e
sadlikeyourdad <57995714+sadlikeyourdad@users.noreply.github.com>
2023-08-25 01:31:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    AOS.init();
+AOS.init({
+    startEvent: 'DOMContentLoaded', // Name of the event dispatched on the document, that AOS should initialize on
+    once: true,  // Whether animation should happen only once - while scrolling down
 });
+
+// Initialize the container with a fade-in effect
+document.querySelector('.container').setAttribute('data-aos', 'fade-up');
