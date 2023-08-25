8ec4fc65892f45b54b566683aa3c09261fa2275b
sadlikeyourdad <samuel.bujnak1@gmail.com>
2023-08-25 18:43:00 +0200

    new

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Any interactivity or animations you'd want to add can be included here.
+  // For now, it's a simple document ready check.
+  console.log("Document loaded!");
+});
